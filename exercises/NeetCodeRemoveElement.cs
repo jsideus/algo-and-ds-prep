8fcfa59eefc8e1bbb6e070e0914e4df396ae7caf
jsideus <heisideus@protonmail.com>
2026-04-09 12:39:03 -0500

    Solved another easy static array exercise

---
+++ exercises/NeetCodeRemoveElement.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+    public int RemoveValues(int[] nums, int val)
+    {
+        var k = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
--- exercises/Program.cs
+++ exercises/Program.cs
@@ -1,7 +1,10 @@
 ﻿int[] consecutiveOnes = new int[10] { 0, 1, 1, 0, 1, 1, 1, 0, 1, 1};
+int[] removeTheOnes = new int[10] {1, 1, 2, 4, 3, 1, 4, 5, 4, 1};
 
+var solution = new Solution();
 var solutions = new Solutions();
 
+Console.WriteLine(solution.RemoveValues(removeTheOnes, 1));
 Console.WriteLine(solutions.FindMaxConsecutiveOnes(consecutiveOnes));
 
 Console.WriteLine("Hello, World!");
