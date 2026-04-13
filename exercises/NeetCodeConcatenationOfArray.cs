ab97261e2b7b1b12a2576af84be3a7071d3804f4
jsideus <heisideus@protonmail.com>
2026-04-13 18:27:41 -0500

    Completed another data structures and algo easy problem with less friction

---
+++ exercises/NeetCodeConcatenationOfArray.cs
@@ -0,0 +1,16 @@
+public class NeetProblemFour
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+
+        int[] ans = new int[2 * nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + nums.Length] = nums[i];
+        }
+
+        return ans;
+    }
+}
--- exercises/Program.cs
+++ exercises/Program.cs
@@ -1,13 +1,17 @@
 ﻿int[] consecutiveOnes = new int[10] { 0, 1, 1, 0, 1, 1, 1, 0, 1, 1};
 int[] removeTheOnes = new int[10] {1, 1, 2, 4, 3, 1, 4, 5, 4, 1};
 int[] removeElements = new int[6] { 2, 4, 5, 3, 1, 2};
+int[] concatElements = new int[4] { 1, 4, 1, 2};
 
 var solution = new Solution();
 var solutions = new Solutions();
 var neetProblemThree = new NeetProblemThree();
+var neetProblemFour = new NeetProblemFour();
 
 Console.WriteLine(solution.RemoveValues(removeTheOnes, 1));
 Console.WriteLine(solutions.FindMaxConsecutiveOnes(consecutiveOnes));
 var result = neetProblemThree.ReplaceElements(removeElements);
+var concatResult = neetProblemFour.GetConcatenation(concatElements);
 Console.WriteLine(string.Join(",", result));
+Console.WriteLine(string.Join(",", concatResult));
 Console.WriteLine("Hello, World!");
