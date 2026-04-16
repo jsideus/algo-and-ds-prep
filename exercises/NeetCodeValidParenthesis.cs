72a19c48997d1136abca78a5f628ae1e2d571799
jsideus <heisideus@protonmail.com>
2026-04-16 17:04:46 -0500

    Solved another stack data structure and algorithm leetcode easy problem

--- exercises/NeetCodeValidParenthesis.cs
+++ exercises/NeetCodeValidParenthesis.cs
@@ -2,6 +2,27 @@ public class NeetCodeValidParentheses
 {
     public bool IsValid(string s)
     {
-        
+        var blah = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+
+            if (c == '(' || c == '[' || c == '{')
+            {
+                blah.Push(c);
+            }
+
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (blah.Count == 0) return false;
+                var top = blah.Pop();
+                if (c == ')' && top != '(') return false;
+                if (c == ']' && top != '[') return false;
+                if (c == '}' && top != '{') return false;
+            }   
+        }
+
+        return blah.Count == 0;
     }
 }
